2d5b179b0b9df625f4c3d6713acf129e50ff1f72
Mukesh Mahato <mukeshmahato843@gmail.com>
2024-03-21 09:05:40 +0545

    login page added

---
+++ resources/views/guest/layouts/header.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description"
+        content="Local Nepal project represents a revolutionary solution that aims to connect tourists
+    from all over the world with local guides, offering a unique and tailored exploration of
+    wonders of Nepal. (Bhattarai, 2023) Considering all these facts, for final year project I
+    have decided to develop an web app hence this proposal is written to address the
+    development of the web app, Local Nepal, which will help the tourists connect with local
+    guide in Nepal which will for sure help them get better experience with their visit. Similarly,
+    local guides and use their spare time to generate some revenue." />
+    @stack('title')
+    {{-- fonts --}}
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    {{-- for icons --}}
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    {{-- core css --}}
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+
+    {{-- page css --}}
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
+
+    <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
+    <script src="{{ asset('assets/js/config.js') }}"></script>
+
+    @stack('links')
+</head>
---
+++ resources/views/guest/layouts/main.blade.php
@@ -0,0 +1,6 @@
+@include('guest.layouts.header')
+
+<body>
+    @yield('main-content')
+    @include('guest.layouts.footer')
+</body>
